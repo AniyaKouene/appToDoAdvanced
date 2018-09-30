510f8c1aaecb6ccc31123340d2b2d4ac53724a4a
Aniya Kouene <aniya.kouene@gmail.com>
2018-09-30 15:19:38 +0200

    cryptage password

--- server/controllers/authentification.js
+++ server/controllers/authentification.js
@@ -1,31 +1,48 @@
 const User = require('../models/user')
 
-const lodash = require('lodash')
+const lodash = require('lodash');
+const jwt = require('jwt-simple');
 
+const config = require('../../config');
 
-exports.signup = function(req, res, next){
+function getTokenForUser(user) {
+    const timeStamp = new Date().getTime();
+    return jwt.encode({
+            sub: user.id,
+            iat: timeStamp
+        },
+        config.secret)
+}
+
+exports.signup = function (req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
-User.findOne({email: email}, function(err, existingUser){
-    if(err){
-        return next(err)
-    }
-    if(existingUser){
-        return res.status(422).send({error: "email deja utilisé"})
-    }
-    if(lodash.isEmpty(email) || lodash.isEmpty(password)){
-        return res.status(422).send({error: "le password est vide"})
-    }else{
-        const user = new User({
-            email: email,
-            password: password
-        });
-        user.save(function(err){
-            if(err){
-                return next(err);
-            }
-            res.json(user);
-        })
-    }
-})
+    User.findOne({
+        email: email
+    }, function (err, existingUser) {
+        if (err) {
+            return next(err)
+        }
+        if (existingUser) {
+            return res.status(422).send({
+                error: "email deja utilisé"
+            })
+        }
+        if (lodash.isEmpty(email) || lodash.isEmpty(password)) {
+            return res.status(422).send({
+                error: "le password est vide"
+            })
+        } else {
+            const user = new User({
+                email: email,
+                password: password
+            });
+            user.save(function (err) {
+                if (err) {
+                    return next(err);
+                }
+                res.json({token: getTokenForUser(user)});
+            })
+        }
+    })
 }
--- server/route.js
+++ server/route.js
@@ -1,5 +1,13 @@
 const authentificationController = require('./controllers/authentification');
+require('./services/passport');
+const passport = require('passport')
+
+const requireToken = passport.authenticate('jwt', {session: false});
 
 module.exports = function (expressServer) {
     expressServer.post("/signup", authentificationController.signup)
-}
+    expressServer.get("/ressourcesSecrete", requireToken, function(req, res){
+        res.send({codeDeLamort: 42})
+    })
+};
+
